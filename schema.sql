8640b331a765524fec77f442a44bf76ed3c792d4
Chad Cook <Charles.Cook28@gmail.com>
2021-01-14 12:10:21 -0600

    updated question array

---
+++ schema.sql
@@ -0,0 +1,37 @@
+drop database if exists employees_db;
+
+create database employees_db;
+
+create table department (
+	id int auto_increment not null,
+    name varchar(30),
+    primary key (id)
+);
+
+create table role (
+	id int auto_increment not null,
+    title varchar(30),
+    salary decimal,
+    department_id int,
+    primary key (id),
+    foreign key (department_id) references department(id)
+);
+
+create table employee (
+	id int auto_increment not null,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id int not null,
+    manager_id int null,
+    primary key (id),
+    foreign key (role_id) references role(id),
+    foreign key (manager_id) references role(id)  
+);
+
+    
+
+    
+
+
+
+
--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const connection = mysql.createConnection({
     multipleStatements: true
   });
 
+const viewAll = () => {
 
+  connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err; console.table(res)
+  })
+  promptUser();
+}
 
 const viewByDepPrompt = () =>
   inquirer.prompt([
@@ -23,39 +29,10 @@ const viewByDepPrompt = () =>
     }
 ]).then(answers => {
   console.info('answer:', answers.department);
-  
+  promptUser();
 })
 
-const viewByManPrompt = () =>
-const accountNumber = request.query.bankAccountNumber;
-
-const statement = `SELECT * FROM bankaccount WHERE accountNumber = ?`;
-console.log(statement);
-
-connection.query(statement, accountNumber, function(error, result) {
-  if (error) return response.json(error);
-
-  if(result.length === 0) return response.json({error: "Sorry bank account not found"});
-
-  return response.json({
-    id: result[0].id,
-    accountNumber: result[0].accountNumber,
-    name: result[0].name,
-  });
-});
-  inquirer.prompt([
-    {
-        type: 'list',
-        name: 'manager',
-        message: 'Which manager do you want to see employees?',
-        // choices: ['Sales', 'Engineering', 'Finance', 'Legal'],
-    }
-]).then(answers => {
-  console.info('answer:', answers.manager);
-  
-})
-
-const addEmpPrompt = () =>
+const addDepPrompt = () => 
   inquirer.prompt([
     {
         type: 'input',
@@ -67,47 +44,65 @@ const addEmpPrompt = () =>
       name: 'last_name',
       message: 'What is the employees last name?'
     },        
-  ])
-
-const viewByManPrompt = () =>
-
-  connection.query
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'remove',
-      message: 'Which employee would you like to remove?',
-      // choices: ['Sales', 'Engineering', 'Finance', 'Legal'],
-    }
-]).then(answers => {
-  console.info('answer:', answers.remove);
-  
+  ]).then(answers => {
+    console.info('answer:', answers.department);
+    connection.query("INSERT INTO employee", function(err, res) {
+      if (err) throw err; console.table(res)
+    })
+  addRolePrompt();
 })
 
-const updateRolePrompt = () =>
+const addRolePrompt = () => 
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the employees first name?'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is the employees last name?'
+    },        
+  ]).then(answers => {
+    console.info('answer:', answers.department);
+    connection.query("INSERT INTO employee", function(err, res) {
+      if (err) throw err; console.table(res)
+    })
+  addEmpPrompt();
+})
+
+const addEmpPrompt = () => 
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the employees first name?'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is the employees last name?'
+    },        
+  ]).then(answers => {
+    console.info('answer:', answers.department);
+    connection.query("INSERT INTO employee", function(err, res) {
+      if (err) throw err; console.table(res)
+    })
+  promptUser();
+})
+
+const updateRolePrompt = () => 
   inquirer.prompt([
     {
       type: 'list',
       name: 'updateRole',
       message: 'Which employee would you like to update their role?',
-      // choices: ['Sales', 'Engineering', 'Finance', 'Legal'],
+      choices: ['Sales', 'Engineering', 'Finance', 'Legal'],
     }
 ]).then(answers => {
   console.info('answer:', answers.updateRole);
-  
-})
-
-const updateManPrompt = () =>
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'updateManager',
-      message: 'Which employee would you like to update their manager?',
-      // choices: [],
-    }
-]).then(answers => {
-  console.info('answer:', answers.updatManager);
-  
+  promptUser();
 })
 
 const promptUser = () =>
@@ -116,30 +111,20 @@ const promptUser = () =>
           type: 'list',
           name: 'tracker',
           message: 'What would you like to do?',
-          choices: ['View all Employees', 'View Employees by Department', 'View Employees by Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager'],
+          choices: ['View all Employees', 'View Employees by Department', 'Add Employee', 'Update Employee Role'],
       }
   ])
   .then(answers => {
       console.info('answer:', answers.tracker);
       if(answers.tracker === 'View all Employees') {
           viewAll()
-        //   .then(answers => {
-        //    //   let manager = new Manager (answers)
-        //  // employees.push(manager)
-        //   })
       } else if (answers.tracker === 'View Employees by Department') {
         viewByDepPrompt();
-      } else if (answers.tracker === 'View Employees by Manager') {
-        viewByManPrompt();
       } else if (answers.tracker === 'Add Employee') {
-        addEmpPrompt();
-      } else if (answers.tracker === 'Remove Employee') {
-        removeEmpPrompt();
+        addDepPrompt();
       } else if (answers.tracker === 'Update Employee Role') {
         updateRolePrompt();
-      } else if (answers.tracker === 'Update Employee Manager') {
-        updateManPrompt();
-      }
+      } 
   })
 
 promptUser();
